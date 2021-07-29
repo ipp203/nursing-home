7bd1881e3ecda2ab0d81e89c48bc0b03d6c08907
ipp203 <ipp203@freemail.hu>
2021-07-29 10:56:44 +0200

    Projekt létrehozása

---
+++ src/test/java/nursinghome/NursingHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package nursinghome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NursingHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
