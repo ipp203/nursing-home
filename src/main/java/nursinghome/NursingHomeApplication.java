a046009e90c9619cc09c022926029c4cdc6aacd5
ipp203 <ipp203@freemail.hu>
2021-07-29 12:02:02 +0200

    pom.xml bovites, application.prop, beanek

--- src/main/java/nursinghome/NursingHomeApplication.java
+++ src/main/java/nursinghome/NursingHomeApplication.java
@@ -1,13 +1,31 @@
 package nursinghome;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NursingHomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NursingHomeApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Nursing Home API")
+                        .version("1.0.0")
+                        .description("Operations with residents"));
     }
 
 }
