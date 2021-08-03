8a7220cf53b11896488c173295217615828a468a
ipp203 <ipp203@freemail.hu>
2021-08-03 10:54:50 +0200

    DateOfBirthValidator message application.properties-bol vett adattal es teszt kulon adatbazissal

--- src/main/java/nursinghome/model/resident/validator/DateOfBirthValidator.java
+++ src/main/java/nursinghome/model/resident/validator/DateOfBirthValidator.java
@@ -1,6 +1,8 @@
 package nursinghome.model.resident.validator;
 
 
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.validation.ConstraintValidator;
@@ -13,14 +15,20 @@ public class DateOfBirthValidator implements ConstraintValidator<DateOfBirth, Lo
 
     private final int minAge;
     private final int maxAge;
+    private String message;
 
     public DateOfBirthValidator(@Value("${nursing-home.resident.minAge}") int minAge, @Value("${nursing-home.resident.maxAge}") int maxAge) {
         this.minAge = minAge;
         this.maxAge = maxAge;
+        message= "Invalid date of birth. Age must be between " + minAge + " and " + maxAge + "!";
     }
 
     @Override
     public boolean isValid(LocalDate dateOfBirth, ConstraintValidatorContext constraintValidatorContext) {
+
+        constraintValidatorContext.disableDefaultConstraintViolation();
+        constraintValidatorContext.buildConstraintViolationWithTemplate(message).addConstraintViolation();
+
         int years = Period.between(dateOfBirth, LocalDate.now()).getYears();
         return (years >= minAge && years <= maxAge);
     }
